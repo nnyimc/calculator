a75f07adc1c26816427f467ebee1d9d7267c2f78
nnyimc <nmncls@orange.fr>
2022-08-30 23:21:25 +0200

    To do: update architecture

--- src/main/java/testing/Calculator.java
+++ src/main/java/testing/Calculator.java
@@ -1,5 +1,8 @@
 package testing;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Calculator implements CalculatorInterface {
 
 	@Override
@@ -20,6 +23,18 @@ public class Calculator implements CalculatorInterface {
 	@Override
 	public int substract(int firstNumber, int secondNumber) {
 		return firstNumber - secondNumber;
+	}
+
+	public Set<Integer> toDigitsSet(int number) {
+		Set<Integer> integers = new HashSet<Integer>();
+		String numberString = String.valueOf(number);
+		
+		for (int i = 0; i < numberString.length(); i++) {
+			if (numberString.charAt(i) != '-') {
+				integers.add( Integer.parseInt("" + numberString.charAt(i),10) );
+			}
+		}
+		return integers;
 	}
 	
 }
--- src/test/java/testing/CalculatorTest.java
+++ src/test/java/testing/CalculatorTest.java
@@ -1,10 +1,24 @@
 package testing;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculatorTest {
+		
+		private Calculator calculator;
+	
+		@BeforeEach
+		public void initCalculator() {
+			calculator = new Calculator();
+		}
 
 		@Test
 		void testAddTwoPositiveNumbers() {
@@ -12,7 +26,6 @@ public class CalculatorTest {
 			// ARRANGE
 			int firstNumber = 6;
 			int secondNumber = 4;
-			Calculator calculator = new Calculator();
 			
 			// ACT
 			int result = calculator.add(firstNumber, secondNumber);
@@ -22,19 +35,44 @@ public class CalculatorTest {
 			
 		}
 		
+		@ParameterizedTest(name = "{0} + {1} doit être égal à {2}")
+		@CsvSource({ "1,1,2", "2,2,4", "3,4,7", "5,7,12"})
+		void testAddTwoNumbers( int firstNumber, int secondNumber, int expectedResult ) {
+			
+			// ARRANGE
+			
+			// ACT
+			int actualResult = calculator.add(firstNumber, secondNumber);
+			
+			// ASSERT
+			assertThat(actualResult).isEqualTo(expectedResult);
+		}
+		
 		@Test
 		void testMultiplyTwoPositiveNumbers() {
 			
 			// ARRANGE 
 			int firstNumber = 4;
 			int secondNumber = 5;
-			Calculator calculator = new Calculator();
 			
 			// ACT
 			int result = calculator.multiply(firstNumber, secondNumber);
 			
 			// ASSERT
-			assertEquals(20, result);
+			assertThat(result).isEqualTo(20);
+		}
+		
+		@ParameterizedTest(name = "{0} x 0 est égal à 0")
+		@ValueSource(ints = { 1, 2, 3, 5, 8, 13 })
+		public void multiplyByZero_shouldReturnZero(int arg) {
+			
+			// ARRANGE
+			
+			// ACT
+			int result = calculator.multiply(arg, 0);
+			
+			// ASSERT
+			assertThat(result).isEqualTo(0);
 		}
 		
 		@Test
@@ -43,14 +81,12 @@ public class CalculatorTest {
 			// ARRANGE 
 			int firstNumber = 8;
 			int secondNumber = 2;
-			Calculator calculator = new Calculator();
 			
 			// ACT
 			int result = calculator.divide(firstNumber, secondNumber);
 			
 			// ASSERT
-			assertEquals(4, result);
-			
+			assertThat(result).isEqualTo(4);
 		}
 		
 		@Test
@@ -59,12 +95,51 @@ public class CalculatorTest {
 			// ARRANGE
 			int firstNumber = 40;
 			int secondNumber = 13;
-			Calculator calculator = new Calculator();
 			
 			// ACT
 			int result = calculator.substract(firstNumber, secondNumber);
 			
 			// ASSERT
-			assertEquals(27, result);
+			assertThat(result).isEqualTo(27);
 		}
+		
+		@Test
+		public void digitsSet_shouldReturnSetOfPositiveIntegers() {
+			
+			// GIVEN
+			int number = 87452;
+			
+			// WHEN
+			Set<Integer> actualDigits = calculator.toDigitsSet(number);
+			
+			// THEN
+			assertThat(actualDigits).containsExactlyInAnyOrder(8,7,4,5,2);
+		}
+		
+		@Test
+		public void digitsSet_shouldAcceptNegativeNumbers() {
+			
+			// GIVEN
+			int number = -2422;
+			
+			// WHEN
+			Set<Integer> actualDigits = calculator.toDigitsSet(number);
+			
+			// THEN
+			assertThat(actualDigits).containsExactlyInAnyOrder(2,4);
+		}
+	
+		@Test
+		public void digitsSet_shouldReturnAlistOfZeros() {
+			
+			// GIVEN
+			int number = 0;
+			
+			// WHEN
+			Set<Integer> actualDigits = calculator.toDigitsSet(number);
+			
+			// THEN
+			assertThat(actualDigits).containsExactly(0);
+		}
+		
 }
